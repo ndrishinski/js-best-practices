aa4604354d46497f14c64de56cfc5e326299b2d2
Nick Drishinski <ndrishinski@yahoo.com>
2019-09-15 16:35:20 -0700

    dont rely on auto semi-colon insertion

--- app.js
+++ app.js
@@ -8,5 +8,16 @@
 'use strict'
 
 const city = 'Chicago'
+// Don't rely on auto semi-colon insertion!
+const address = {
+    street: '45th Ave',
+    number: '3442',
+    city: 'Boston'
+};
 
+function newFunc() {
+    console.log('proper semi-colon usage');
+};
+
+newFunc();
 console.log(city);
